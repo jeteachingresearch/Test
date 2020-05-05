4a57096e0e32d8057c268f810db2070a1203ab48
jeteachingresearch <55083985+jeteachingresearch@users.noreply.github.com>
2020-05-05 16:52:51 +0200

    Add files via upload

---
+++ Outer2.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package outerinner;
+
+
+public class Outer {
+
+    public String get() {
+        return "Outer";
+    }
+
+    private  class Middle {
+
+        public String get() {
+            return "Middle";
+        }
+
+        public class Inner {
+
+            public String get() {
+                return "Inner";
+            }
+
+            public String exec() {
+                return Outer.this.get();
+            }
+        }
+
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+        System.out.println(outer.new Middle().new Inner().exec());
+    }
+
+}
