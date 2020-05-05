a85c08e56d6d41f0f7117a020b7260e6ab9453ba
jeteachingresearch <55083985+jeteachingresearch@users.noreply.github.com>
2020-05-05 17:17:43 +0200

    Update Outer.java

--- Outer.java
+++ Outer.java
@@ -8,12 +8,12 @@ package outerinner;
 
 public class Outer {
 
+    public empty(){}
+    
     public String get(int a, int b, int c, int d, int e, int f, int g, int h) {
-        int aa;
-        int bb;
         return "Outer";
     }
-
+    
     private  class Middle {
 
         public String get() {
