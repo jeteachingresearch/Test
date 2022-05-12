d4afc2100cf389f47b298106d5d5b8dd34fcd0c4
jeteachingresearch <55083985+jeteachingresearch@users.noreply.github.com>
2022-05-12 12:15:10 +0200

    Update Outer2.java

--- Outer2.java
+++ Outer2.java
@@ -8,7 +8,7 @@ package outerinner;
 
 public class Outer {
 
-    public String get(int a, int b, int c, int d, int e, int f, int g, int h) {
+    public String get(int a, int b) {
         return "Outer";
     }
 
